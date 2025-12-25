3cd1854201b6c58539cd2df98889da9e37654daf
Marios Kanellopoulos <mariosknl@outlook.com>
2025-12-25 09:02:09 +0200

    Add Sanity schema types for products, orders, customers, and categories; include constants for filters and order statuses; update package.json and pnpm-lock.yaml to include @sanity/icons; add sample data and configuration files for Shadcn and Clerk integration.

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div className="">
       {/* Featured Products Carousel */}
-      <Button>Click me</Button>
+
       {/* Page Banner */}
+
       {/* Category Tiles */}
+
       {/* Products Section */}
     </div>
   );
