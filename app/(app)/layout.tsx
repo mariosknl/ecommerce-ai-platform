5055e65380a2264df5abc12787bebd9584dc4b6e
Marios Kanellopoulos <mariosknl@outlook.com>
2025-12-29 08:42:27 +0200

    Add checkout functionality with Stripe integration; implement CheckoutClient, CheckoutButton, and related components; enhance layout with CartSheet and Header; add loading states and skeletons for product and checkout pages; update package.json and pnpm-lock.yaml for new dependencies.

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,14 +2,21 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { SanityLive } from "@/sanity/lib/live";
 import { CartStoreProvider } from "@/lib/store/cart-store-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Header } from "@/components/app/Header";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
+import { CartSheet } from "@/components/app/CartSheet";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
       <CartStoreProvider>
-        <main>{children}</main>
-        <Toaster position="bottom-center" />
-        <SanityLive />
+        <ChatStoreProvider>
+          <Header />
+          <main>{children}</main>
+          <CartSheet />
+          <Toaster position="bottom-center" />
+          <SanityLive />
+        </ChatStoreProvider>
       </CartStoreProvider>
     </ClerkProvider>
   );
